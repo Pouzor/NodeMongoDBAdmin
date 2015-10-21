7a4c0a7bb04c0122922b72ac30f3adbc2db6fc79
Remy Jardinet <remy.jardinet.ext@francetv.fr>
2015-10-21 17:37:26 +0200

    Base template

--- packages/custom/nodemongodbadmin/public/controllers/nodemongodbadmin.js
+++ packages/custom/nodemongodbadmin/public/controllers/nodemongodbadmin.js
@@ -15,12 +15,27 @@ angular.module('mean.nodemongodbadmin').controller('NodemongodbadminController',
 angular.module('mean.nodemongodbadmin').controller('DatabaseCtrl', ['$scope','$stateParams', 'Database',
     function ($scope, $stateParams, Database) {
 
+        $scope.databaseSelected = $stateParams.name;
+        $scope.collections = Database.get({id: $scope.databaseSelected});
+        $scope.request = '{}';
+        $scope.type = "find";
+        $scope.limit = 10;
+        $scope.sort_key = "_id";
+        $scope.sort_value = -1;
+        $scope.collectionSelected = 'none';
+        $scope.result = null;
+        $scope.sending = '';
+        $scope.error = false;
+        $scope.info = false;
+
         Database.query(function (databases) {
             $scope.databases = databases;
         });
 
-        $scope.databaseSelected = $stateParams.name;
-        $scope.collections = Database.get({id: $scope.databaseSelected});
+        $scope.selectCollection = function (name) {
+            //  $scope.collection_selected = name.substring($scope.database_selected.length + 1);
+            $scope.collectionSelected = name;
+        }
 
     }
 ]);
